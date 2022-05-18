deaf196a6d7fe28932f27a1ee94b04ce996bee77
Dago Peralta <dagoperalta163@gmail.com>
2022-05-18 16:38:08 -0500

    Add Splash for empty activity with Logo solicited

---
+++ app/src/main/java/com/example/imdb/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.imdb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        finish()
+    }
+}
