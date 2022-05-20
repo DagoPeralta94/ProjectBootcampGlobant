f054e9541f9f038fdf49f3f458c5bc816467d435
Dago Peralta <dagoperalta163@gmail.com>
2022-05-19 20:03:35 -0500

    Add GlideCirckeCrop to MainActivity

--- app/src/main/java/com/example/imdb/MainActivity.kt
+++ app/src/main/java/com/example/imdb/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.imdb
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.request.RequestOptions
 import com.example.imdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,14 +21,18 @@ class MainActivity : AppCompatActivity() {
         binding.txtRegister.setOnClickListener { goingToRegister() }
         binding.login1.setOnClickListener { goingToNavigation() }
 
+
         Glide.with(this)
             .load(R.drawable.logo_apple)
+            .apply(RequestOptions.circleCropTransform())
             .into(binding.imApple)
         Glide.with(this)
             .load(R.drawable.logo_facebook)
+            .apply(RequestOptions.circleCropTransform())
             .into(binding.imFacebook)
         Glide.with(this)
             .load(R.drawable.logo_google)
+            .apply(RequestOptions.circleCropTransform())
             .into(binding.imGoogle)
 
     }
