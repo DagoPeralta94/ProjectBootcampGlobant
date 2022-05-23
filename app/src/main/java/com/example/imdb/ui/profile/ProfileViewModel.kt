04dd951b53af5412046ad5a0ef20f705703a659e
Dago Peralta <dagoperalta163@gmail.com>
2022-05-23 06:05:45 -0500

    Add new botton Navigation and Change names on btsN

--- app/src/main/java/com/example/imdb/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/imdb/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.imdb.ui.dashboard
+package com.example.imdb.ui.notifications
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is search Fragment"
+        value = "This is Profile Fragment"
     }
     val text: LiveData<String> = _text
 }
