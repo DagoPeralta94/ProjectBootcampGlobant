109e0f5587f427a65b22048d6dca721a7a424c89
DagoPeraltaImexhs <dagoberto.peralta@imexhs.com>
2022-06-30 10:40:30 -0500

    Add item_portada_movies and ReciclerView

--- app/src/main/java/com/example/imdb/HomeDashboard.kt
+++ app/src/main/java/com/example/imdb/HomeDashboard.kt
@@ -17,30 +17,30 @@ class HomeDashboard : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.botton_navigation)
 
-        bottomNavigationView.selectedItemId = R.id.home2
+            bottomNavigationView.selectedItemId = R.id.home2
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
-           when (item.itemId) {
-               R.id.search2 -> {
-                   startActivity(Intent(applicationContext, SearchActivity::class.java))
-                   overridePendingTransition(0,0)
-                   return@OnNavigationItemSelectedListener true
-               }
-               R.id.home2 -> return@OnNavigationItemSelectedListener true
-               R.id.play2 -> {
-                   startActivity(Intent(applicationContext, PlayDashboard::class.java))
-                   overridePendingTransition(0,0)
-                   return@OnNavigationItemSelectedListener true
-               }
-               R.id.profile2 -> {
-                   startActivity(Intent(applicationContext, ProfileDashboard::class.java))
-                   overridePendingTransition(0,0)
-                   return@OnNavigationItemSelectedListener true
-               }
-           }
-            false
-        })
-    }
+            bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.search2 -> {
+                        startActivity(Intent(applicationContext, SearchActivity::class.java))
+                        overridePendingTransition(0, 0)
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.home2 -> return@OnNavigationItemSelectedListener true
+                    R.id.play2 -> {
+                        startActivity(Intent(applicationContext, PlayDashboard::class.java))
+                        overridePendingTransition(0, 0)
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.profile2 -> {
+                        startActivity(Intent(applicationContext, ProfileDashboard::class.java))
+                        overridePendingTransition(0, 0)
+                        return@OnNavigationItemSelectedListener true
+                    }
+                }
+                false
+            })
+        }
     override fun onStop() {
         super.onStop()
         finish()
