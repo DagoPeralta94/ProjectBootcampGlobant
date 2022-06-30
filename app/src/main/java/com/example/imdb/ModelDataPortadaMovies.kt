2db52e87f089bc2587be954d0e674559a97015a4
DagoPeraltaImexhs <dagoberto.peralta@imexhs.com>
2022-06-30 11:09:35 -0500

    Add DataModelPortadaMovies

---
+++ app/src/main/java/com/example/imdb/ModelDataPortadaMovies.kt
@@ -0,0 +1,8 @@
+package com.example.imdb
+
+data class ModelDataPortadaMovies(
+    val urlPhotoPortadaFragment: String,
+    val urlPhotoPortadaMovie: String,
+    val titleMoviePortada: String,
+    val descriptionMoviePortada: String
+)
--- app/src/main/java/com/example/imdb/MoviesPortadaProvider.kt
+++ app/src/main/java/com/example/imdb/MoviesPortadaProvider.kt
@@ -1,6 +1,15 @@
 package com.example.imdb
 
-data class MoviesPortadaProvider(
-    val
-)
+class MoviesPortadaProvider {
+    companion object {
+        val moviesListPortada = listOf<ModelDataPortadaMovies>(
+            ModelDataPortadaMovies(
+                "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg",
+                "Stranger Things",
+                "Trailer Oficial"
+
+            ),
+        )
+    }
 }
